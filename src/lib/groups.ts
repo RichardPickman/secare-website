98fb9183dd345315a294abc7371f76b659856d09
RichardPickman <drezzerock@gmail.com>
2026-05-02 21:06:33 +0200

    rename groups and add theme related images

--- src/lib/groups.ts
+++ src/lib/groups.ts
@@ -1,18 +1,30 @@
-import home1 from "../assets/screenshots/home-1.png";
-import home2 from "../assets/screenshots/home-2.png";
+import homeLight1 from "../assets/screenshots/home-light-1.png";
+import homeLight2 from "../assets/screenshots/home-light-2.png";
 
-import subscriptions1 from "../assets/screenshots/subscriptions-1.png";
-import subscriptions2 from "../assets/screenshots/subscriptions-2.png";
+import homeDark1 from "../assets/screenshots/home-dark-1.png";
+import homeDark2 from "../assets/screenshots/home-dark-2.png";
 
-import watch1 from "../assets/screenshots/watch-light-1.png";
-import watch2 from "../assets/screenshots/watch-light-2.png";
+import subscriptionsLight1 from "../assets/screenshots/subscriptions-light-1.png";
+import subscriptionsLight2 from "../assets/screenshots/subscriptions-light-2.png";
+
+import subscriptionsDark1 from "../assets/screenshots/subscriptions-dark-1.png";
+import subscriptionsDark2 from "../assets/screenshots/subscriptions-dark-2.png";
+
+import watchLight1 from "../assets/screenshots/watching-light-1.png";
+import watchLight2 from "../assets/screenshots/watching-light-2.png";
+
+import watchDark1 from "../assets/screenshots/watching-dark-1.png";
+import watchDark2 from "../assets/screenshots/watching-dark-2.png";
 
 export const groups = [
     {
         title: "Home",
         description:
             "Create a clean and focused YouTube homepage by disabling unwanted elements such as the header, Shorts, tags, and promotional sections. Secare allows you to remove visual clutter and recommendations that distract from the main feed, helping you browse more efficiently.",
-        images: [home1, home2],
+        images: {
+            light: [homeLight1, homeLight2],
+            dark: [homeDark1, homeDark2],
+        },
         settings: {
             shorts: ["Hide shorts in main feed", "Hide shorts in menu"],
             header: [
@@ -33,7 +45,10 @@ export const groups = [
         title: "Subscriptions",
         description:
             "Simplify your Subscriptions feed by disabling relevant videos, Shorts, and other distracting elements. Have a better control over the page layout, so you can focus on the channels you actually follow.",
-        images: [subscriptions1, subscriptions2],
+        images: {
+            light: [subscriptionsLight1, subscriptionsLight2],
+            dark: [subscriptionsDark1, subscriptionsDark2],
+        },
         settings: {
             shorts: ["Hide shorts in main feed", "Hide shorts in menu"],
             header: [
@@ -49,7 +64,10 @@ export const groups = [
         title: "Watching",
         description:
             "Enhance your video watching experience by hiding the sidebar, description elements, comments and channel controls. Secare lets you remove distractions on the right side and below the player, allowing for a more immersive and focused viewing session.",
-        images: [watch1, watch2],
+        images: {
+            light: [watchLight1, watchLight2],
+            dark: [watchDark1, watchDark2],
+        },
         settings: {
             shorts: ["Hide shorts in main feed", "Hide shorts in menu"],
             header: [
